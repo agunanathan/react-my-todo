17f2cb01bcff7826d6031b8172c14dfa7dfec85f
agunanathan <asokan.gunanathan@gmail.com>
2023-02-28 12:43:58 -0500

    getting modal to work

--- src/App.js
+++ src/App.js
@@ -1,66 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
-import TodoList from './Components/TodoList';
-import {v4 as uuidv4} from 'uuid';
-
-
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  },[todos])
-
-  function handleClearCompletedTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
-  function toogleTodo(id){
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
-
-  function deleteTodo(id){
-    let newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.deleted = true
-    newTodos = todos.filter(todo => !todo.deleted)
-    setTodos(newTodos)
-  }
-
-  function handleKeydown(event) {
-    if (event.key === 'Enter') {
-      handleAddTodo();
-    }
-  }
-
-  function handleAddTodo() {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete:false}]
-    })
-    todoNameRef.current.value=null
-  }
-
   return (
-    <>
-    <input aria-label='input todo' ref={todoNameRef} type="text" onKeyDown={handleKeydown} autoFocus/>
-    <button onClick={handleAddTodo}>Add Todo</button>
-    <button onClick={handleClearCompletedTodos}>Clear Completed Todos</button> 
-    <div> You have {todos.filter(todo => !todo.complete).length} todos to complete</div>
-    <TodoList todos={todos} toggleTodo={toogleTodo} deleteTodo={deleteTodo}/>
-    </>
-  )
+    <div className="App">
+      <div className="todolist">
+        <TodoList />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,12 +1,90 @@
-import React from 'react'
-import Todo from './Todo'
+import React, {useState, useEffect} from 'react'
+import Modal from './Modal';
 
-export default function TodoList( {todos, toggleTodo, editTodo, deleteTodo }) {
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+const TodoList = () => {
+
+  const [isModalOpen, setModalOpen] = useState(false);
+  const[todo, setTodo] = useState("");
+  const[todos, setTodos] = useState([]);
+  const[editId, setEditId] = useState(0);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  },[todos])
+
+  const handleEditTodo = (selectedTodoId) => {
+    setModalOpen(true);
+    const selectedTodoObject = todos.find((item) => item.id === selectedTodoId);
+    setTodo(selectedTodoObject.todo);
+    setEditId(selectedTodoId);
+  }
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    if (todo !== "") {
+    setTodos([{id: `todo-${Date.now()}` , todo }, ...todos]) //date.now will produces unique number by give current time in millisecond
+    setTodo("");
+}
+};
+  const handleSaveTodo = (e) =>{
+        e.preventDefault();  
+     
+      const updatedTodos = todos.map((item)=>item.id===editId ? {id: todo.id, todo} :item);
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      setModalOpen(false);
+    
+  }
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos ([...delTodo]);
+  };
+
+  function toogleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+ 
   return (
-    
-    todos.map(todo => {
-        return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} editTodo={editTodo} deleteTodo={deleteTodo} />
-    })  
-    
+    <div>
+      <h1>Todo List </h1>
+      <form className= "todolist" onSubmit={handleAddTodo}>
+        <input type= "text" placeholder='Todo'  autoFocus value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button type= "submit"> {false? "Edit" : "Add"}</button>
+      </form>
+      <ul className= "todolist"> 
+      {
+        todos.map((item) => (
+        <li className = "todoitem"> 
+        <span key={item.id}>{item.todo} </span>
+        <input aria-label="checkbox" type="checkbox" checked={todo.complete} onChange={toogleTodo}/>
+        <button onClick={() => handleEditTodo(item.id)}>Edit</button>
+        <button onClick={() => handleDelete(item.id)}> Delete</button>
+        </li>
+        ))
+      }
+     </ul>
+     {isModalOpen &&
+     <Modal> 
+      <form className= "todolist" onSubmit={handleSaveTodo}>
+        <input  aria-label="edit input" type= "text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <button type= "submit"> Save</button>
+        <button type= "button" onClick={()=>setModalOpen(false)}> Cancel</button>
+     </form>
+      </Modal>
+     }
+    </div>
   )
 }
+
+export default TodoList
